f7738f2f24b489e981ced2663096f1a68bf770f4
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-12-06 16:07:41 +0600

    user update bug updated

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import decodedUser from "../../helpers/decodedUser";
+import { Roles } from "../auth/auth.constant";
 import { userServices } from "./user.service";
 
 const getUsers = async (req: Request, res: Response) => {
@@ -19,12 +21,20 @@ const getUsers = async (req: Request, res: Response) => {
 
 const updateUser = async (req: Request, res: Response) => {
 	try {
-		const result = await userServices.updateUser(req.body, req.params.userId!);
-		return res.status(200).json({
-			success: true,
-			message: "User updated successfully",
-			data: result.rows[0],
-		});
+		const currentUser = decodedUser(req);
+		if (currentUser.role === Roles.admin || currentUser.id === req.params.userId) {
+			const result = await userServices.updateUser(req, req.params.userId!);
+			return res.status(200).json({
+				success: true,
+				message: "User updated successfully",
+				data: result.rows[0],
+			});
+		} else {
+			return res.status(401).json({
+				success: false,
+				message: "Unauthorized access!",
+			});
+		}
 	} catch (error) {
 		res.status(500).json({
 			success: false,
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import { Request } from "express";
 import { pool } from "../../config/db";
 
 const getUsers = async () => {
@@ -8,9 +9,8 @@ const getUsers = async () => {
 	return result;
 };
 
-const updateUser = async (payload: Record<string, unknown>, userId: string) => {
-	const { name, email, phone, role } = payload;
-
+const updateUser = async (req: Request, userId: string) => {
+	const { name, email, phone, role } = req.body;
 	const result = await pool.query(
 		`UPDATE users SET name = $1, email = LOWER($2), phone = $3, role = $4 WHERE id = $5 RETURNING name, email, phone, role, id`,
 		[name, email, phone, role, userId]
