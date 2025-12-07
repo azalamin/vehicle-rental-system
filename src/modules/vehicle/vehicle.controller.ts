42e2c7382a693b3a1d54739041e8449e59bf9992
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-12-07 11:40:45 +0600

    vehicle updated

--- src/modules/vehicle/vehicle.controller.ts
+++ src/modules/vehicle/vehicle.controller.ts
@@ -20,7 +20,15 @@ const createVehicle = async (req: Request, res: Response) => {
 const getAllVehicles = async (req: Request, res: Response) => {
 	try {
 		const result = await vehicleServices.getAllVehicles();
-		res.status(200).json({
+		if (result.rows.length === 0) {
+			return res.status(200).json({
+				success: true,
+				message: "No vehicles found",
+				data: result.rows,
+			});
+		}
+
+		return res.status(200).json({
 			success: true,
 			message: "Vehicles retrieved successfully",
 			data: result.rows,
@@ -36,10 +44,18 @@ const getAllVehicles = async (req: Request, res: Response) => {
 const getSingleVehicle = async (req: Request, res: Response) => {
 	try {
 		const result = await vehicleServices.getSingleVehicle(req.params.vehicleId as string);
-		res.status(200).json({
+		if (result.rows.length === 0) {
+			return res.status(200).json({
+				success: true,
+				message: "Vehicle not found",
+				data: result.rows,
+			});
+		}
+
+		return res.status(200).json({
 			success: true,
 			message: "Vehicle retrieved successfully",
-			data: result.rows,
+			data: result.rows[0],
 		});
 	} catch (error) {
 		res.status(500).json({
